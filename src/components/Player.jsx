e15ecab0822ab7ac36c5fb1565ebeff7c2381df6
Rishabh Bajpai <rishabh@Rishabhs-MacBook-Air.local>
2024-12-06 05:09:00 +0530

    added edit/save handler for editing and saving player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditAndSave() {
+    isEditing ? setIsEditing(false) : setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let buttonText = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+    buttonText = "Save";
+  }
+
   return (
     <li>
       <span className="payer">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditAndSave}>{buttonText}</button>
     </li>
   );
 }
