fcf395366af30eda0a77c398fe545b1792061bbf
Rishabh Bajpai <rishabh@Rishabhs-MacBook-Air.local>
2024-12-06 05:39:28 +0530

    best practice to update state based on previous state value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,14 +4,16 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditAndSave() {
-    isEditing ? setIsEditing(false) : setIsEditing(true);
+    // To update state using previous state value use arrow function like below
+    // as react will provide the latest previous value of the state
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
   let buttonText = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
     buttonText = "Save";
   }
 
